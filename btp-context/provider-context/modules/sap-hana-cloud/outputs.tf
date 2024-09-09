40e841fc1f70ab597c6291e8cda2523592930759
ptesny <ptesny@wanadoo.fr>
2024-09-09 15:29:46 +0100

    Update outputs.tf

--- btp-context/provider-context/modules/sap-hana-cloud/outputs.tf
+++ btp-context/provider-context/modules/sap-hana-cloud/outputs.tf
@@ -23,32 +23,32 @@ output "provider_k8s" {
   value = local_sensitive_file.provider_sm
 }
 
-# subaccount data collection
+# provider-context (subaccount) data collection
 
 # We output the data that we need in order to import the resources in the next step.
 output "subaccount_name" {
   description = "The name of the subaccount."
-  value       = data.btp_subaccount.context.my_account.name
+  value       = data.btp_subaccount.context.name
 }
 
 output "subaccount_region" {
   description = "The region of the subaccount."
-  value       = data.btp_subaccount.context.my_account.region
+  value       = data.btp_subaccount.context.region
 }
 
 output "subaccount_subdomain" {
   description = "The subdomain of the subaccount."
-  value       = data.btp_subaccount.context.my_account.subdomain
+  value       = data.btp_subaccount.context.subdomain
 }
 
 output "subaccount_usage" {
   description = "The usage of the subaccount."
-  value       = data.btp_subaccount.context.my_account.usage
+  value       = data.btp_subaccount.context.usage
 }
 
 output "subaccount_labels" {
   description = "The labels of the subaccount."
-  value       = data.btp_subaccount.context.my_account.labels
+  value       = data.btp_subaccount.context.labels
 }
 
 output "postgresql_db" {
