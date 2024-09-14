6ecf11b65d01ce3154d1e8e3b75fe8ec03fcd0d3
ptesny <ptesny@wanadoo.fr>
2024-09-14 21:47:54 +0100

    Update bootstrap-postgresql-trial.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-postgresql-trial.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-postgresql-trial.tf
@@ -51,7 +51,7 @@ locals {
 	        "servicePlanName": "trial",
 	        "parameters": {
 	            "region": "us-east-1",
-	            "allow_access": "${local.ips.ips}"  //"52.6.160.101"
+	            "allow_access": "${local.ips}"  //"52.6.160.101"
 	        }
 	    }	
 	})
