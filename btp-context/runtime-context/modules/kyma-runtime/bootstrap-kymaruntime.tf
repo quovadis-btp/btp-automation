bcab99368f8c130f6dbc876270ae77e68bba6ef9
ptesny <ptesny@wanadoo.fr>
2024-09-14 22:21:43 +0100

    Update bootstrap-kymaruntime.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-kymaruntime.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-kymaruntime.tf
@@ -561,7 +561,7 @@ resource "terraform_data" "egress_ips" {
 
     PostgreSQL='${self.input}'
     echo $(jq -r '.' <<< $PostgreSQL)
-    echo $PostgreSQL | jq -r -R -s --arg ips "$CLUSTER_IPS" '.spec.parameters |= . + { region: "us-east-1", allow_access: $ips }'
+    echo $PostgreSQL | jq -r --arg ips "$CLUSTER_IPS" '.spec.parameters |= . + { region: "us-east-1", allow_access: $ips }'
      )
    EOF
  }
