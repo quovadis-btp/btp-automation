0f4c60acd4b35dcbc99796d2072422baddd35198
ptesny <ptesny@wanadoo.fr>
2024-09-19 13:44:05 +0100

    Update bootstrap-kymaruntime.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-kymaruntime.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-kymaruntime.tf
@@ -601,7 +601,7 @@ data "tfe_outputs" "provider_context" {
 // this provider context can be null
 locals {
   remote_backend = one(data.terraform_remote_state.provider_context[*].outputs.provider_k8s)
-  tfe_backend    = one(data.tfe_outputs.provider_context[*].outputs.provider_k8s)
+  tfe_backend    = one(data.tfe_outputs.provider_context[*].provider_k8s)
 
   provider_k8s = local.remote_backend != null ? jsonencode(local.remote_backend) : jsonencode(local.tfe_backend)
 
