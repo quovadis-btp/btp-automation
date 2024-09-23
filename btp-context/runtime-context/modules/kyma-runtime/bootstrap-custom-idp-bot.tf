2a99e0921999bd68f89d2ced3f24cb1cf1bd00d3
ptesny <ptesny@wanadoo.fr>
2024-09-23 22:29:13 +0100

    Update bootstrap-custom-idp-bot.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-bot.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-bot.tf
@@ -372,7 +372,7 @@ locals {
 #
 resource "kubectl_manifest" "OpenIDConnect_PROD" {
 //    depends_on = [ module.runtime_context.kubeconfig_prod_exec ]
-    depends_on = [ terraform_data.bootstrap-kymaruntime-bot, output.kubeconfig_prod_exec ]
+    depends_on = [ terraform_data.bootstrap-kymaruntime-bot ]
 
     yaml_body  = yamlencode(jsondecode(local.OpenIDConnect_PROD))
     server_side_apply = true
@@ -381,7 +381,7 @@ resource "kubectl_manifest" "OpenIDConnect_PROD" {
 
 resource "kubectl_manifest" "OpenIDConnect_STAGE" {
 //    depends_on = [ module.runtime_context.kubeconfig_prod_exec ]
-    depends_on = [ terraform_data.bootstrap-kymaruntime-bot, output.kubeconfig_prod_exec ]
+    depends_on = [ terraform_data.bootstrap-kymaruntime-bot ]
 
     yaml_body  = yamlencode(jsondecode(local.OpenIDConnect_STAGE))
     server_side_apply = true
