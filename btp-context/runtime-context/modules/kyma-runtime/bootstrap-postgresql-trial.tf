9516423a9dd60bef5ea9ebfd85f5ad15fa7fc62f
ptesny <ptesny@wanadoo.fr>
2024-09-28 17:00:21 +0100

    Update bootstrap-postgresql-trial.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-postgresql-trial.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-postgresql-trial.tf
@@ -221,7 +221,6 @@ resource "terraform_data" "egress_ips" {
     for zone in $ZONES; do
     overrides="{ \"apiVersion\": \"v1\", \"spec\": { \"nodeSelector\": { \"topology.kubernetes.io/zone\": \"$zone\" } } }"
     echo | ./kubectl run --kubeconfig $KUBECONFIG --timeout=5m -i --tty curl --image=everpeace/curl-jq --restart=Never  --overrides="$overrides" --rm --command -- curl -s http://ifconfig.me/ip >> temp_ips.txt 2>/dev/null
-    //echo | ./kubectl delete pod/curl --kubeconfig $KUBECONFIG --force --ignore-not-found
     sleep 2
     done
     cat temp_ips.txt
