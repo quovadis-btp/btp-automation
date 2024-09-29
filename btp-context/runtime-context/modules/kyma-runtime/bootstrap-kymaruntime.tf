3581a1b882d9984304c8fbb99f1892981da54af3
ptesny <ptesny@wanadoo.fr>
2024-09-29 10:14:43 +0100

    Update bootstrap-kymaruntime.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-kymaruntime.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-kymaruntime.tf
@@ -246,20 +246,25 @@ data "http" "kubeconfig" {
     min_delay_ms = 1000
   }
   
+  /*
   lifecycle {
     postcondition {
       condition     = can(regex("kind: Config",self.response_body))
       error_message = "Invalid content of downloaded kubeconfig"
     }
-  }
+  }*/
 
-/*
+
   lifecycle {
+    precondition {
+      condition     = contains([200, 201, 204], data.http.kubeconfig.status_code)
+      error_message = data.http.kubeconfig.response_body
+    }
     postcondition {
       condition     = contains([200, 201, 204], self.status_code)
       error_message = self.response_body
     }
-  }  */
+  } 
 }
 
 
