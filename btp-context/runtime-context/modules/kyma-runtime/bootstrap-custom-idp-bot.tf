a3dd9c8272eebdd8ff4a577c0cb4f9b74206a9ba
ptesny <ptesny@wanadoo.fr>
2024-10-09 00:03:55 +0100

    Update bootstrap-custom-idp-bot.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-bot.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-bot.tf
@@ -740,7 +740,8 @@ data "jq_query" "gh_workflow" {
 
    data = local.gh_workflow
 //   query = ". | .jobs[].steps[0].with |= . + { kubeconfig: ${data.jq_query.kubeconfig_gh_exec.result}   }"
-   query = ". | .jobs[].steps[0].with |= . + { kubeconfig: ${local.kubeconfig_gh_json} | tostring  }"
+//   query = ". | .jobs[].steps[0].with |= . + { kubeconfig: ${local.kubeconfig_gh_json} | tostring  }"
+   query = ". | .jobs[].steps[0].with |= . + { kubeconfig: ${local.kubeconfig_gh_json} | tojson  }"
 }
 
 output "gh_workflow_json" {
