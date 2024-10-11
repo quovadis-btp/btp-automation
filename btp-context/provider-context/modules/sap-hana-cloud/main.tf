6793157c0dff44b10596fa6cca7413f46080ecaf
ptesny <ptesny@wanadoo.fr>
2024-10-11 08:52:27 +0100

    Update main.tf

--- btp-context/provider-context/modules/sap-hana-cloud/main.tf
+++ btp-context/provider-context/modules/sap-hana-cloud/main.tf
@@ -484,7 +484,7 @@ data "external" "openssl_cert" {
 
 locals {
 //  hc-x509-p12 = data.external.openssl_cert.result
-  hc-x509-p12 = jsondecode(data.external.openssl_cert.result)
+  hc-x509-p12 = jsonencode(data.external.openssl_cert.result)
 }
 
 output "dest-httpbin" {
