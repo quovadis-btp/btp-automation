3a65ce77d976cd8cc8da9f1050adcd6011b95aa5
ptesny <ptesny@wanadoo.fr>
2024-10-11 10:30:57 +0100

    Update main.tf

--- btp-context/provider-context/modules/sap-hana-cloud/main.tf
+++ btp-context/provider-context/modules/sap-hana-cloud/main.tf
@@ -441,6 +441,17 @@ locals {
 }
 
 
+locals {
+  is_windows = length(regexall("^[a-z]:", lower(abspath(path.root)))) > 0
+}
+
+output "absolute_path" {
+    value = abspath(path.root)
+}
+
+output "operating_system" {
+    value = local.is_windows ? "Windows" : "Linux"
+}
 
 /*
 # https://stackoverflow.com/a/15490765
