17380e0ba4c5496eadd86524d3a83941c80799ae
ptesny <ptesny@wanadoo.fr>
2024-10-14 21:56:25 +0100

    Update bootstrap-custom-idp-k8s.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-k8s.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-k8s.tf
@@ -110,7 +110,7 @@ resource "btp_subaccount_service_binding" "ias-local-binding-cert" {
     key-length      = 4096
     validity        = 1
     validity-type   = "DAYS"
-    app-identifier  = "kymaruntime"
+    app-identifier  = "kymaruntime-idp"
   })
   lifecycle {
     replace_triggered_by = [
