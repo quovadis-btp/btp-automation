c16161e7e6c679b66209627add7022a03ce1d480
ptesny <ptesny@wanadoo.fr>
2024-10-15 19:25:07 +0100

    Update bootstrap-custom-idp-bot.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-bot.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-bot.tf
@@ -764,7 +764,7 @@ data "jq_query" "gh_workflow" {
    depends_on = [data.http.kubeconfig]
 
    data = local.gh_workflow
-   query = ". | .jobs[].steps[0].with |= . + "{\"kubeconfig\":${data.jq_query.kubeconfig_gh_exec.result}\\n\"}"
+   query = ". | .jobs[].steps[0].with |= . + "\"kubeconfig\":${data.jq_query.kubeconfig_gh_exec.result}\\n\""
 //   query = ". | .jobs[].steps[0].with |= . + ${local.kubeconfig_gh_json} "
 //   query = ". | .jobs[].steps[0].with |= . + { kubeconfig: ${data.jq_query.kubeconfig_gh_exec.result}   }"   
 
