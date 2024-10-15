b2f93679bca0cfd24a6fa904f9bebb8730b771e3
ptesny <ptesny@wanadoo.fr>
2024-10-15 20:31:40 +0100

    Update bootstrap-custom-idp-bot.tf

--- btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-bot.tf
+++ btp-context/runtime-context/modules/kyma-runtime/bootstrap-custom-idp-bot.tf
@@ -756,7 +756,7 @@ locals {
 */
 
 //  kubeconfig_gh_json = format("%s%s%s", "\"kubeconfig\":",data.jq_query.kubeconfig_gh_exec.result,"\\n")
-  kubeconfig_gh_json = format("%s%s%s", "{\"kubeconfig\":",data.jq_query.kubeconfig_gh_exec.result,"\\n\"}")
+  kubeconfig_gh_json = format("%s%s%s", "{\"kubeconfig\":\"",data.jq_query.kubeconfig_gh_exec.result,"\\n\"}")
 }
 
 output "kubeconfig_gh_json" {
