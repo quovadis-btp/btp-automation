f872b4a673b5e6d6300c9ec476e96a251e0e7ab9
ptesny <ptesny@wanadoo.fr>
2024-10-16 19:49:40 +0100

    Update main.tf

--- btp-context/provider-context/modules/sap-hana-cloud/main.tf
+++ btp-context/provider-context/modules/sap-hana-cloud/main.tf
@@ -100,9 +100,9 @@ output "has_postgresql_binding" {
 }
 
 data "btp_subaccount_service_binding" "postgresql" {
-//  count          = var.BTP_POSTGRESQL_PLAN != "trial" ? 0 : 1
+  count          = var.BTP_POSTGRESQL_PLAN != "trial" ? 0 : 1
 //  count          =  local.postgresql-binding == {} ? 0 : 1
-  count          =  var.HAS_POSTGRESQL_BINDING ? 1 : 0
+//  count          =  var.HAS_POSTGRESQL_BINDING ? 1 : 0
 
   subaccount_id = data.btp_subaccount.context.id
   name          = "postgresql-binding"  
