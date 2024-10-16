fc3beba7ffea3bd78da3901ffe4ae7ca63adbdd7
ptesny <ptesny@wanadoo.fr>
2024-10-16 13:36:33 +0100

    Update versions.tf

--- btp-context/bootstrap-context/modules/custom-idp/versions.tf
+++ btp-context/bootstrap-context/modules/custom-idp/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.9.3"
+  required_version = ">= 1.9"
 
   required_providers {
     btp = {
       source  = "SAP/btp"
-      version = "~>1.7.0"
+      version = "1.7.0"
     }
   }
 }
